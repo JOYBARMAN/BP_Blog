b96de260101b49917ce206457f21beb28ec6548f
JOYBARMAN <barmanjoy26@yahoo.com>
2023-12-20 14:42:08 +0600

    Update tag serializers

--- tag/rest/serializers/tag.py
+++ tag/rest/serializers/tag.py
@@ -4,37 +4,35 @@ from rest_framework import serializers
 from tag.models import Tag
 
 
+base_fields = [
+    "id",
+    "uid",
+    "name",
+    "description",
+    "status",
+]
+
+base_read_only_fields = [
+    "id",
+    "uid",
+]
+
+
 class TagListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = [
-            "id",
-            "uid",
-            "name",
-            "description",
-            "status"
-        ]
-        read_only_fields = [
-            "id",
-            "uid",
-        ]
+        fields = base_fields
+        read_only_fields = base_read_only_fields
 
 
 class TagDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = [
-            "id",
-            "uid",
-            "name",
-            "description",
-            "status",
+        fields = base_fields + [
             "created_at",
             "updated_at",
         ]
-        read_only_fields = [
-            "id",
-            "uid",
+        read_only_fields = base_read_only_fields + [
             "created_at",
-            "updated_at"
+            "updated_at",
         ]
